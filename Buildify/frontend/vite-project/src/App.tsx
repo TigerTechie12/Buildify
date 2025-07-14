535c1c03a61f5c27938dc26a4f321552228085e7
Shreyash <shreyashiitr22@gmail.com>
2025-07-14 19:20:16 +0530

    added frontend pages and components

--- Buildify/frontend/vite-project/src/App.tsx
+++ Buildify/frontend/vite-project/src/App.tsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Builder } from './pages/Builder';
+import { parseXml } from './steps';
 
 function App() {
-
-
   return (
-    <>
-  
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/builder" element={<Builder />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
